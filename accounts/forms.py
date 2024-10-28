2b11e692b91cf767689d92b29e1975b36a91c887
DanielRebolledo1 <da.rebolledo@duocuc.cl>
2024-10-28 00:02:28 -0300

    Registro de usario, template de login

---
+++ accounts/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from .models import Account
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Ingresar Contraseña',
+        'class' : 'form-control'
+    }))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Confirmar Contraseña'
+    }))
+
+    class Meta:
+        model = Account
+        fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
+        
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+        
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "Contraseña ingresada no coincide"
+            )
+  
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Ingresar Nombre'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Ingresar Apellido'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Ingresar Número de teléfono'
+        self.fields['email'].widget.attrs['placeholder'] = 'Ingresar email'
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
